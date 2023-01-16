72693ee3293512207e2085acbd5c46fd87d9d534
Kaung Khant Ko Ko <59255928+Kaung-Khant-Ko-Ko@users.noreply.github.com>
2023-01-16 19:13:23 +0630

    Update helper.py

--- src/helper.py
+++ src/helper.py
@@ -2,7 +2,7 @@ from transformers import TFAutoModelForSeq2SeqLM, AutoTokenizer, pipeline
 
 
 def load_model():
-    return TFAutoModelForSeq2SeqLM.from_pretrained("../base_model", local_files_only = True), AutoTokenizer.from_pretrained("../base_tokenizer", local_files_only = True)
+    return TFAutoModelForSeq2SeqLM.from_pretrained("../base_model/", local_files_only = True), AutoTokenizer.from_pretrained("../base_tokenizer/", local_files_only = True)
 
 def predict_summary(model, tokenizer, news):
     summarizer = pipeline("summarization", model = model, tokenizer = tokenizer, framework = "tf")
